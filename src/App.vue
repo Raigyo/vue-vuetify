f9b5c2d060ba90b7ee81e8d554ff078bf1510f0a
chilot <https://github.com/Raigyo>
2021-10-27 18:13:59 +0200

    delete task

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,39 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="https://picsum.photos/1920/1080?random"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+      <v-toolbar-title>Vuetify Todo</v-toolbar-title>
 
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet id="scrolling-techniques-5" max-height="600"> </v-sheet>
     <v-main>
       <router-view></router-view>
     </v-main>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,73 @@
 <template>
-  <task />
+  <div class="home">
+    <v-list flat class="pt-0">
+      <v-subheader>List of tasks</v-subheader>
+      <v-divider></v-divider>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+            <v-list-item-action>
+              <!-- modifier 'stop' to not affect the parent -->
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
 </template>
 
 <script>
-import Task from "../components/Task";
+// import Task from "../components/Task";
 
 export default {
   name: "Home",
 
-  components: {
-    Task,
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake up",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Wake up2",
+          done: false,
+        },
+        {
+          id: 3,
+          title: "Wake up3",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
   },
 };
 </script>
